8ce9b512e81ae16b78caa2bf812a0e361323da09
lniquensuarez-collab <lniquensuarez@gmail.com>
2026-06-12 16:26:00 -0500

    Update gemini-code-1781196451752.py

--- gemini-code-1781196451752.py
+++ gemini-code-1781196451752.py
@@ -121,7 +121,7 @@ def entrenar_ia(_df):
     }
     return clf, le, h_mods, a_mods, team_stats
 
-st.title("🏆 ULTIMATE AI V14.4")
+st.title("🏆 ULTIMATE AI")
 st.markdown("### Predicciones IA (Datos Reales 2 Años)")
 
 df_global = cargar_y_enriquecer_selecciones()
