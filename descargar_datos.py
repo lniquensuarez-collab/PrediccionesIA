29186e1741a27fce3343d1f15ee3b5f2b04a49d7
lniquensuarez-collab <lniquensuarez@gmail.com>
2026-06-18 11:25:34 -0500

    Update descargar_datos.py

--- descargar_datos.py
+++ descargar_datos.py
@@ -23,6 +23,9 @@ COMPETICIONES = {
     "Eliminatorias UEFA": 32,
     "Eliminatorias CONCACAF": 33, # Norte y Centroamérica (México, USA, Haití)
     "Eliminatorias AFC": 35,      # Asia (Japón, Corea, Irak)
+    "Eliminatorias AFC": 32,       # <- Añadir para Japón, Corea del Sur, Arabia
+    "Copa Oro": 22,                # <- Torneo continental de CONCACAF
+    "Copa Asiática": 18            # <- Torneo continental de Asia
     "Eliminatorias CAF": 29,      # África (Eliminatorias Mundialistas)
     "UEFA Nations League": 5,     # Torneo europeo (Noruega, Suecia, etc.)
     "Friendlies": 10              # Amistosos Internacionales de la FIFA
